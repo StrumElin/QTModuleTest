2a6b191f9b2bf6c2566e4f9a83bccb8a04486b48
zhoupengkai <15556532221@163.com>
2018-11-29 19:33:47 +0800

    0.0.1

---
+++ QTModuleTest/QTModuleTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QTModuleTest
+//
+//  Created by ☺strum☺ on 2018/11/29.
+//  Copyright © 2018年 l. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QTModuleTest/QTModuleTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QTModuleTest
+//
+//  Created by ☺strum☺ on 2018/11/29.
+//  Copyright © 2018年 l. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
